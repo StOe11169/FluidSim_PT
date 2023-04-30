dbac079cea198a57b9734b8e54e6824ccb37caca
Stefan Oelbracht <stefan.oelbracht@studmail.w-hs.de>
2023-04-30 18:52:27 +0200

    deleted now redundant files

--- canvasdrawings.js
+++
@@ -1,4 +0,0 @@
-//new "scene" start with clearing the canvas
-//establish coordinates of outlines o shapes
-//find equation to compare those coords too an given cell
-//if within those outlines = wall
